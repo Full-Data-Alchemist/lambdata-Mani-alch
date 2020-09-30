c6ad72a22380472ef10bf91bd435e6214c4cbb7c
Full_Data_Alchemist <imani.beasley.72@gmail.com>
2020-09-29 22:04:37 -0500

    class update

--- lambdata-Mani-alch/day_1.py
+++ lambdata-Mani-alch/day_1.py
@@ -7,32 +7,39 @@ from sklearn.model_selection import train_test_split
 import shap
 
 
+class Manis_alchemy:
+    """class for my two hjelper funtions"""
 
-def splitter(df):
-    """function splits a dataframe"""
-    df = df.copy() #protectg og dataframe
-    train, test = train_test_split(df, 
-                                        train=.8,
-                                        test=.2)
+    def __init__(self):
+        pass
 
-    X = train
-    y = test
-    return X, y
 
-def shap_val(row, model, encoder):
-    """Function returns shap values"""
-    row = X_test.loc[[row]]
-    row
+    def splitter(self, df):
+        """function splits a dataframe"""
+        df = df.copy() #protectg og dataframe
+        train, test = train_test_split(df, 
+                                            train=.8,
+                                            test=.2)
 
-    explainer = shap.TreeExplainer(model)
-    encoder = encoder
-    row_processed = encoder.transform(row)
-    shap_values = explainer.shap_values(row_processed)
+        X = train
+        y = test
+        return X, y
 
-    shap.initjs()
-    return shap.force_plot(
-        base_value=explainer.expected_value,
-        shap_values=shap_values,
-        features=row,
-        link='logit')
+    def shap_val(self, row, model, encoder):
+        """Function returns shap values"""
+        row = X_test.loc[[row]]
+        row
+
+        explainer = shap.TreeExplainer(model)
+        encoder = encoder
+        row_processed = encoder.transform(row)
+        shap_values = explainer.shap_values(row_processed)
+
+        shap.initjs()
+        return shap.force_plot(
+            base_value=explainer.expected_value,
+            shap_values=shap_values,
+            features=row,
+            link='logit')
+    
         
